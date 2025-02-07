7c420e5df0c18cab980b893d528e364995db5512
kumsu <kumsu@King_Vishwa>
2025-02-07 09:42:26 +0530

    OnScheduleExecution

---
+++ dummymaven_proj/src/test/java/dummymaven_proj/OnScheduleExecutionTest.java
@@ -0,0 +1,19 @@
+package dummymaven_proj;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class OnScheduleExecutionTest {
+
+	public class PollScmTest {
+
+		public class PollSCMTest {
+			@Test
+			public void Sample() {
+				Reporter.log("JenkinsTest OnScheduleExecution Class Executed", true);
+			}
+
+		}
+
+	}
+}
