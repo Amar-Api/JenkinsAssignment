6c1707b3ec81b65cfe9e9a6f7a5bf79ec3bf0893
kumsu <kumsu@King_Vishwa>
2025-02-07 09:25:14 +0530

    DummyMaven

---
+++ dummymaven_proj/src/test/java/dummymaven_proj/PollScmTest.java
@@ -0,0 +1,15 @@
+package dummymaven_proj;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollScmTest {
+
+	public class PollSCMTest {
+		@Test
+		public void Sample() {
+			Reporter.log("JenkinsTest PollSCMTest Class Executed", true);
+		}
+
+	}
+}
